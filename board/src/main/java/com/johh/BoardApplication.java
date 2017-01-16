5ec3480f549f5fcb11bf10237d5847c163e82e9a
hy2hh <hanghee3@gmail.com>
2017-01-16 18:00:10 +0900

    init~_~

--- board/src/main/java/com/johh/BoardApplication.java
+++ board/src/main/java/com/johh/BoardApplication.java
@@ -1,9 +1,9 @@
 package com.johh;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 public class BoardApplication {
 
 	public static void main(String[] args) {
